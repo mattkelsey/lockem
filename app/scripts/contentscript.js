945594dce56ed804d3bf958de3587b1bedf70386
mattkelsey <mattkelseymk@gmail.com>
2016-07-29 00:53:09 -0700

    Add element grabbing and credential reading

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,3 +1,59 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Content script');
+var elements = {};
+
+var userListener = function(e) {
+    console.log("document body clicked");
+    e = e || window.event;
+    if(e.target.tagName == "INPUT") {
+        elements.user = e.target;
+        document.removeEventListener('click', userListener, false);
+    } else {
+        alert("Please select an input element... That is a " + e.target.tagName + " element.");
+    }
+}
+var passListener = function(e) {
+    console.log("document body clicked");
+    e = e || window.event;
+    if(e.target.tagName == "INPUT") {
+        elements.pass = e.target;
+        document.removeEventListener('click', passListener, false);
+    } else {
+       alert("Please select an input element... That is a " + e.target.tagName + " element.");
+    }
+}
+chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+    console.log("message recieved");
+    if(request.event == "selection") {
+        aquireElements(request.type);
+    } else if (request.event == "read") {
+        getCredentials(elements);
+    } else {
+        recover();
+    }
+});
+
+//writeCredentials(null, null, null);
+
+
+function aquireElements(elementType) {
+    if(elementType == "user") {
+        document.addEventListener('click', userListener, false);
+    } else if (elementType == "pass") {
+        document.addEventListener('click', passListener, false);
+    } else {
+        recover();
+    }
+}
+
+function getCredentials(elements) {
+    var creds = {};
+    creds.user = elements.user.value;
+    creds.pass = elements.pass.value;
+    console.log(creds);
+    //write credentials
+}
+
+function recover() {
+    console.log("something went very, very wrong");
+}
--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,26 @@
 'use strict';
+document.getElementById("grabUser").addEventListener("click", userClick);
+document.getElementById("grabPass").addEventListener("click", passClick);
+document.getElementById("grabCreds").addEventListener("click", credsClick);
 
-console.log('\'Allo \'Allo! Popup');
+var query = { active: true, currentWindow: true };
+function userClick() {
+    chrome.tabs.query(query, (tabs) => {
+        var tab = tabs[0];
+        chrome.tabs.sendMessage(tab.id, { event:"selection", type:"user" });
+    });
+}
+
+function passClick() {
+    chrome.tabs.query(query, (tabs) => {
+        var tab = tabs[0];
+        chrome.tabs.sendMessage(tab.id, { event:"selection", type:"pass" });
+    });
+}
+
+function credsClick() {
+    chrome.tabs.query(query, (tabs) => {
+        var tab = tabs[0];
+        chrome.tabs.sendMessage(tab.id, { event:"read" });
+    });
+}
